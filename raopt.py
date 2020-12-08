68a3f9ce4ea540e6ddcb52ec23858319ec0f9eb6
hamza <hamza.kacem@ensi-uma.tn>
2020-12-08 15:34:09 +0100

    select forst extended test done :).

--- raopt.py
+++ raopt.py
@@ -5,17 +5,19 @@ import radb.ast
 import radb.parse
 
 dd = {"Person": {"name": "string", "age": "integer", "gender": "string"}, "Eats": {"name": "string", "pizza": "string"}}
-stmt = "\select_{Person.gender = 'f' and Person.age = 16 and Person.name = 'Amy' and c = d} Person;"
-stmt_result = "\select_{Person.gender = 'f'} \select_{Person.age = 16} \select_{Person.name = 'Amy'} \select_{c = d} Person;"
+stmt = "\select_{E.pizza = 'mushroom' and E.price < 10} \\rename_{E: *}(Eats);"
+stmt_result = "\select_{E.pizza = 'mushroom'} \select_{E.price < 10} \\rename_{E: *}(Eats);"
 ra = radb.parse.one_statement_from_string(stmt)
 ra_result = radb.parse.one_statement_from_string(stmt_result)
+# print(ra)
+# print(ra_result)
+# print('yup1')
 
-valExpreBinary_list_ra = ra.cond.inputs
-inputs_ra = ra.inputs[0]
-print(ra_result)
 
+def break_select(ra):
+    """ Breaks up Complex selection into simple selection operations."""
 
-def rule_break_up_selections(ra):
+    # valExpreBinary_list_ra will contains all the valExpreBinaryOp object with the right order
     valExpreBinary_list_ra = [ra.cond.inputs[1]]
     inputs_0 = ra.cond.inputs[0]
     prev_inputs_0 = None
@@ -23,19 +25,35 @@ def rule_break_up_selections(ra):
         valExpreBinary_list_ra.append(inputs_0.inputs[1])
         prev_inputs_0 = inputs_0
         inputs_0 = inputs_0.inputs[0]
-    if prev_inputs_0 != None:
+    if type(inputs_0.inputs[0]) != radb.ast.ValExprBinaryOp and prev_inputs_0 == None:
+        valExpreBinary_list_ra.append(inputs_0)
+    elif prev_inputs_0 != None:
         valExpreBinary_list_ra.append(prev_inputs_0.inputs[0])
-
+    # now
     select_index_number = len(valExpreBinary_list_ra)  # in orde to not include the last valExprBinaryOp
     res = radb.ast.Select(cond=valExpreBinary_list_ra[0], input=ra.inputs[0])
-    for i in range(1, select_index_number - 1):
+    # it was for i in range(1, select_index_number - 1):
+    for i in range(1, select_index_number):
         res = (radb.ast.Select(cond=valExpreBinary_list_ra[i], input=res))
+    return res
+    # return radb.ast.Select(cond=valExpreBinary_list_ra[-1], input=res)
 
-    return radb.ast.Select(cond=valExpreBinary_list_ra[-1], input=res)
+
+def rule_break_up_selections(ra):
+    if isinstance(ra, radb.ast.Select):
+        return break_select(ra)
+    elif isinstance(ra, radb.ast.Project):
+        return radb.ast.Project(attrs=ra.attrs, input=break_select(ra.inputs[0]))
+    elif isinstance(ra, radb.ast.Cross):
+        if isinstance(ra.inputs[0], radb.ast.Select):
+            return radb.ast.Cross(break_select(ra.inputs[0]), ra.inputs[1])
+        elif isinstance(ra.inputs[1], radb.ast.Select):
+            return radb.ast.Cross(ra.inputs[0],break_select(ra.inputs[1]))
 
 
-r = rule_break_up_selections(ra)
-print(r)
+# L = rule_break_up_selections(ra)
+# print(L)
+# print('yup2')
 
 # def rule_push_down_selections(ra, dd):
 #     pass
